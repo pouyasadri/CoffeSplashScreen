2ea4338d64aec951a820b131c246777c2cdee11d
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-10 14:57:53 +0200

    Initial Commit

---
+++ CoffeSplashScreen/CoffeSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeSplashScreenApp.swift
+//  CoffeSplashScreen
+//
+//  Created by Pouya Sadri on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
